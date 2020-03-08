77aaf56629cef1dee2945e303d51476d50dce031
KennyGha <32459150+KennyGha@users.noreply.github.com>
2020-03-08 14:03:11 +0800

    Add files via upload

---
+++ kubernetes/run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+#. ./make_prediction.sh
+# Step 1:
+# Build image and add a descriptive tag
+sudo docker build -f Dockerfile . -t sklearnapp --label sklearnapp.
+# Step 2:
+# List docker images
+docker image ls --filter label=sklearnapp
+
+# Step 3:
+# Run flask app
+docker run -it -p 5000:5000 sklearnapp
